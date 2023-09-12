189deec9ee86b4bac8fdad098a3367a9963ec95e
ikram483 <ikramlabhaji2019@gmail.com>
2023-09-12 14:06:24 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,16 +1,16 @@
-    #include "main.h"
-    #include <stdio.h>
+#include "main.h"
+#include <stdio.h>
 
-    /**
-    
-    *
-    * Return: Always 0.
-    */
-    int main(void)
-    {
-        int n;
+/**
+ 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
 
-        n = add(89, 9);
-        printf("%d\n", n);
-        return (0);
-    }
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
